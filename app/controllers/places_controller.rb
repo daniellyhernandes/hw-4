e09650d8b95b8b1f483ceb1a4316d72b70585b11
Danielly Hernandes <danielly.hernandes@kellogg.northwestern.edu>
2023-06-05 01:17:11 +0000

    Fixed Home Logic

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,16 +2,21 @@ class PlacesController < ApplicationController
 
   def index
     #If user is logged in, allow to create the post + send the place_id to the front
-    if @current_user
+    # if @current_user
       @places = Place.all
-    else
-      redirect_to "/login"  
-    end
+    # else
+    #   redirect_to "/login"  
+    # end
   end
 
   def show
+    if @current_user
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"]}).and(Post.where({ "user_id" => @current_user["id"]}))
+  else
+    flash["notice"] = "You must login to see posts"
+    redirect_to "/login"  
+  end
 
   end
 
